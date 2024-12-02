d31555373368037c06f98bdb7a90f28f218ec05b
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2024-12-02 14:23:52 +0530

    Initial Commit

---
+++ build/CMakeFiles/raze.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raze.dir/src/main.cpp.o"
+  "CMakeFiles/raze.dir/src/main.cpp.o.d"
+  "raze"
+  "raze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
